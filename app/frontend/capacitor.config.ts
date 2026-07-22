6df76194776c1e810fcc99471e4540804daf3392
George <ladariugeo@gmail.com>
2026-07-22 15:47:57 -0500

    Aded N

--- app/frontend/capacitor.config.ts
+++ app/frontend/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.socialautopty.app',
-  appName: 'IA Community Manager',
+  appName: 'AI Marketing Creator',
   webDir: 'dist',
   server: {
     androidScheme: 'https',
